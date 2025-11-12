56a2f6bfed485c1f005c1f87051e85d1f2d74443
Lusufa <rohitthakur2006rt2006@gmail.com>
2025-11-12 17:12:52 +0530

    commit

--- src/app/(staticPages)/contact/page.tsx
+++ src/app/(staticPages)/contact/page.tsx
@@ -24,7 +24,7 @@ export default function ContactPage() {
         href="mailto:Noteriousmu@gmail.com"
         className="font-excon rounded-lg border-2 border-black bg-zinc-100 px-8 py-4 text-2xl font-bold tracking-wider text-black shadow-[4px_4px_0px_0px_#000] transition-all duration-200 hover:bg-black hover:text-white dark:border-white/20 dark:bg-zinc-900 dark:text-white dark:shadow-[4px_4px_0px_0px_#757373] dark:hover:bg-white dark:hover:text-black"
       >
-        Noteriousmu@gmail.com
+        rohitthakur2006rt2006@gmail.com
       </a>
     </section>
   );
